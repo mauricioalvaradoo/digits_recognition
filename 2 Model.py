64a91b685baf98aa9b656a1598c7791a54dd6bb5
Mauricio Alvarado <99780331+mauricioalvaradoo@users.noreply.github.com>
2023-01-09 00:21:28 -0500

    Versión #3 modelo con activ. lineal

--- 2 Model.py
+++ 2 Model.py
@@ -1,19 +1,20 @@
 # Hand-Written Digits Recognition
 import numpy as np
 import pandas as pd
-from numba import njit
-from sklearn.metrics import confusion_matrix, ConfusionMatrixDisplay
+import matplotlib.pyplot as plt
+# from numba import njit
 
 import tensorflow as tf
 from tensorflow.keras.layers import Dense
 from tensorflow.keras import Sequential, Input
 from tensorflow.keras.losses import SparseCategoricalCrossentropy
 from tensorflow.keras.optimizers import Adam
-# import warnings
-# warnings.simplefilter(action='ignore', category=FutureWarning)
+
+import warnings
+warnings.simplefilter(action='ignore', category=FutureWarning)
 
 
-# Data
+# Data =====================================================================
 X_train = pd.read_pickle("./data/X_train.pkl")
 y_train = pd.read_pickle("./data/y_train.pkl")
 X_test = pd.read_pickle("./data/X_test.pkl")
@@ -35,50 +36,63 @@ Se usará una arquitectura neuronal que consiste en lo siguiente:
 * Layer3 = 1 neuronas
 
 """""
+
+
 tf.random.set_seed(19)
+
 model = Sequential(
     [
         Input(shape = (784,)),
         Dense(units = 50, activation = "sigmoid", name = "Layer1"),
         Dense(units = 20, activation = "sigmoid", name = "Layer2"),
-        Dense(units = 10,  activation = "softmax", name = "Layer3")
+        Dense(units = 10,  activation = "linear", name = "Layer3") # En vez de "softmax"
 
     ], name = "Modelo1"
 )
 
 # model.summary()
 
-
 model.compile(
-    loss = SparseCategoricalCrossentropy(),
+    loss = SparseCategoricalCrossentropy(from_logits=True),
     optimizer = Adam(0.01),
 )
 
 
 results = model.fit(
     X_train, y_train,
-    epochs = 100,
+    epochs = 500,
     verbose = False
-)
+) ### Que epochs message aparezca 10 veces
 
-# GRAFICO DE PERDIDA!
-import matplotlib.pyplot as plt
-plt.xlabel("#Epoca")
-plt.ylabel("Magnitud de perdida")
-plt.plot(results.history["loss"])
 
-#MATRIZ DE CONFUSION
-y_hat=results.predict(X_test)
+logits = model.predict(X_train)
+fx = tf.nn.softmax(logits)
 
-y_hat = np.where(y_hat > 0.5, 1, 0)
-conf = confusion_matrix(y_test, y_hat)
 
-conf_plot = ConfusionMatrixDisplay(
-    confusion_matrix = conf,
-)
+# Check historia -> parametros
 
-conf_plot.plot()
 
-# HACER CROSS VALIDATION 
+# Función de pérdida =======================================================
+plt.figure(figsize=(6,4))
 
-# GUARDAR MODELO
+plt.plot(results.history["loss"], color="black")
+
+plt.xlabel("# Iteraciones")
+plt.ylabel("Magnitud")
+plt.title("Magnitud de la función de pérdida por iteraciones")
+
+plt.savefig("./figures/log-loss.pdf")
+plt.show()
+
+
+# Cross-validation =========================================================
+
+
+
+
+
+# Guardar modelo ===========================================================
+
+
+
+
